98858ee479b8e40b2535c58d85dab7d5f32bae8c
Blake DeGraw <bbdegraw@gmail.com>
2021-04-27 10:25:24 -0700

    did '+1' array in python

--- solutions.py
+++ solutions.py
@@ -1,3 +1,35 @@
+# Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+# the array can't be empty
+# only non-negative, single digit integers are allowed
+# Return nil (or your language's equivalent) for invalid inputs.
+
+# Examples
+# For example the array [2, 3, 9] equals 239, adding one would return the array [2, 4, 0].
+
+# [4, 3, 2, 5] would return [4, 3, 2, 6]
+
+def up_array(arr):
+    if len(arr) == 0:
+        return None
+    for i in arr:
+        if i < 0 or type(i) != int or i > 9:
+            return None
+    x = len(arr) - 1
+    while x > -1:
+        arr[x] = arr[x] +1
+        if arr[x] > 9:
+            arr[x] = 0
+            x -= 1
+        else:
+            x = -1
+    if arr[0] == 0:
+        arr.insert(0, 1)
+    return arr
+
+
+
+
 # // Check if given chord is minor or major.
 
 # // Rules:
