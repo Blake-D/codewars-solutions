b00c4588ea22b26b3d0f60e689ec7fa2057e936e
Blake DeGraw <bbdegraw@gmail.com>
2021-05-21 10:32:42 -0700

    today's challenge. It looked kinda easy, but I figured there might be a curve ball somewhere. there wasn't though. it was easy

--- solutions.js
+++ solutions.js
@@ -1,3 +1,17 @@
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+function inArray(array1,array2){
+  let answerArray = []
+  for(let i = 0; i < array2.length; i++){
+    for(let j = 0; j < array1.length; j++){
+      if(array2[i].includes(array1[j]) && !answerArray.includes(array1[j])){
+        answerArray.push(array1[j])
+      }
+    }
+  }
+  return answerArray.sort()
+}
+
 // Your job is to write a function which increments a string, to create a new string.
 
 // If the string already ends with a number, the number should be incremented by 1.
