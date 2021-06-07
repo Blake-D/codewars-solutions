8294a818bac4c99cb8dee9dacda40b4d77b61d30
Blake DeGraw <bbdegraw@gmail.com>
2021-06-07 12:31:15 -0700

    had time for a second challenge. I thought maybe there was a curveball nested somewhere in there, but it was as easy as it looked

--- solutions.js
+++ solutions.js
@@ -1,3 +1,30 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+function parse(data){
+  let returnArray = []
+  let x = 0
+  for(let i = 0; i < data.length; i++){
+    if(data[i] === "i"){
+      x++
+    } else if(data[i] === "d"){
+      x--
+    } else if(data[i] === "s"){
+      x = x * x
+    } else if(data[i] === "o"){
+      returnArray.push(x)
+    }
+  }
+  return returnArray
+}
+
 // Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string. Empty string values should be ignored. Empty arrays or null/nil values being passed into the method should result in an empty string being returned.
 
 // formatWords(['ninja', 'samurai', 'ronin']) // should return "ninja, samurai and ronin"
