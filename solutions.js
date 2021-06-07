0861917d79cb57317094cd4251fc840a6dba5a51
Blake DeGraw <bbdegraw@gmail.com>
2021-06-07 12:56:10 -0700

    was kind of easy (it is a monday adter all), but was a nice reminder about exception handling

--- solutions.js
+++ solutions.js
@@ -1,3 +1,25 @@
+// Build up a method that takes an integer and formats it to a 'time - like' format.
+
+// The method must raise an exception if its hour length is less than 3 digits and greater than 4.
+
+// Example:
+// solution(800); // should return '8:00'
+// solution(1000); // should return '10:00'
+// solution(1451); // should return '14:51'
+// solution(3351); // should return '33:51'
+// solution(10000); // should raise an exception
+
+function solution(hour) {
+  hour = hour.toString()
+  if(hour.length > 2 && hour.length < 5){
+    hour = hour.split('')
+    hour.splice(-2, 0, ":")
+    return hour.join('')
+  } else{
+    throw "Error: input is either too long or too short"
+  }
+}
+
 // Write a simple parser that will parse and run Deadfish.
 
 // Deadfish has 4 commands, each 1 character long:
