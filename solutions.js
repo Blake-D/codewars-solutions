b85dd6d1822f4e8c649593b45e6ac66990829f27
Blake DeGraw <bbdegraw@gmail.com>
2021-06-15 11:30:49 -0700

    back on the wagon after a week or so of prepping for my TA work. Today's solution...kinda cheeky. I like it.

--- solutions.js
+++ solutions.js
@@ -1,3 +1,28 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+function sortArray(array) {
+  let odds = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 !== 0){
+      odds.push(array[i])
+      array[i] = "x"
+    }
+  }
+  odds.sort((a, b) => a - b)
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === 'x'){
+      array[i] = odds[0]
+      odds.shift()
+    }
+  }
+  return array
+}
+
 // Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
 
 // pyramid(0) => [ ]
