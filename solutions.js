347dbe0b736f8264c4c5c0200e6c1a191d6a5e09
Blake DeGraw <bbdegraw@gmail.com>
2021-06-25 09:17:18 -0700

    needed a little bit more of a challenge to start off my day

--- solutions.js
+++ solutions.js
@@ -1,3 +1,30 @@
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+function meeting(s) {
+  s = s.toUpperCase().split(';')
+  let temp
+  for(let i = 0; i < s.length; i++){
+    s[i] = s[i].split(':')
+    temp = s[i][0]
+    s[i][0] = s[i][1]
+    s[i][1] = temp
+    s[i] = '(' + s[i].join(', ') + ')'
+  }
+  return s.sort().join('')
+}
+
 // In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
 // If anything in the text isn't a letter, ignore it and don't return it.
